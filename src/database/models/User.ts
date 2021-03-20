5ac4f9a16ca34a96f97132fb4a4e5a570a9aea13
DanielGustavo <dg520589@gmail.com>
2021-03-19 23:30:58 -0300

    feat(backend): create a model 'User' and a migration to table 'users'

---
+++ src/database/models/User.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  is_a_school!: boolean;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  avatar_filename!: string;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+}
+
+export default User;
