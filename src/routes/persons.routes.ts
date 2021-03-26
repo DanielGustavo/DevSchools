2e0bbc766400c32f8e1c8bebc4d5b071750827b6
DanielGustavo <dg520589@gmail.com>
2021-03-26 00:16:36 -0300

    feature(backend/createUserService): require the creator of the 'Person' to be a school

--- src/routes/persons.routes.ts
+++ src/routes/persons.routes.ts
@@ -5,14 +5,20 @@ import createUserValidator from '../validators/createUser.validator';
 
 import createPersonService from '../services/createPerson.service';
 
+import ensureAuthorizationMiddleware from '../middlewares/ensureAuthorization.middleware';
+
 const router = Router();
 
 router.post(
   '/persons',
+  ensureAuthorizationMiddleware,
   createUserValidator,
   createPersonValidator,
   async (request: Request, response: Response) => {
-    const person = await createPersonService(request.body);
+    const person = await createPersonService({
+      personDatas: request.body,
+      creatorDatas: request.user,
+    });
 
     Object.assign(person.user, { id: undefined, password: undefined });
 
--- src/services/createPerson.service.ts
+++ src/services/createPerson.service.ts
@@ -4,26 +4,43 @@ import Person from '../database/models/Person';
 
 import createUserService from './createUser.service';
 
+import AppError from '../errors/AppError';
+
 interface Request {
-  username: string;
-  role: string;
-  name: string;
-  password: string;
+  personDatas: {
+    username: string;
+    role: string;
+    name: string;
+    password: string;
+  };
+  creatorDatas: {
+    id: string;
+    isASchool: Boolean;
+  };
 }
 
 export default async function createPersonService(
   request: Request
 ): Promise<Person> {
+  const { personDatas, creatorDatas } = request;
+
+  const creatorIsntASchool = !creatorDatas.isASchool;
+
+  if (creatorIsntASchool) {
+    const errorMsg = 'You are not allowed to create a user of type "person"';
+    throw new AppError(403, errorMsg);
+  }
+
   const personRepository = getRepository(Person);
 
   const user = await createUserService({
-    ...request,
+    ...personDatas,
     isASchool: false,
   });
 
   const person = personRepository.create({
-    role: request.role,
-    name: request.name,
+    role: personDatas.role,
+    name: personDatas.name,
     user,
   });
 
