0a5ddd0718c763af6fae62fefd5b2aee176d308e
DanielGustavo <dg520589@gmail.com>
2021-05-11 21:50:05 -0300

    feat(backend/subjectsController): add method 'edit'

--- src/controllers/subjects.controller.ts
+++ src/controllers/subjects.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from 'express';
 
 import createSubjectService from '../services/createSubject.service';
+import updateASubjectByIdService from '../services/updateASubjectById.service';
 
 class SubjectsController {
   async store(request: Request, response: Response) {
     const subject = await createSubjectService({
       schoolId: request.user.school?.id as string,
       title: request.body.title,
+    });
+
+    return response.json(subject);
+  }
+
+  async edit(request: Request, response: Response) {
+    const subject = await updateASubjectByIdService({
+      schoolId: request.user.school?.id as string,
+      newTitle: request.body.newTitle,
+      subjectId: request.params.subjectId,
     });
 
     return response.json(subject);
