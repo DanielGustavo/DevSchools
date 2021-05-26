914c2cafa02aa33c2e88fb2bcc859076f509f9f5
DanielGustavo <dg520589@gmail.com>
2021-05-25 21:06:21 -0300

    feat(backend): add DELETE route '/persons/:personId'

--- src/routes/persons.routes.ts
+++ src/routes/persons.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import createPersonValidator from '../validators/createPerson.validator';
 import createUserValidator from '../validators/createUser.validator';
 import listClassroomsOfAPersonValidator from '../validators/listClassroomsOfAPerson.validator';
+import deleteAPersonValidator from '../validators/deleteAPerson.validator';
 
 import ensureAuthorizationMiddleware from '../middlewares/ensureAuthorization.middleware';
 import ensureIsASchoolMiddleware from '../middlewares/ensureIsASchool.middleware';
@@ -13,18 +14,25 @@ const router = Router();
 
 router.use('/persons', ensureAuthorizationMiddleware);
 
+router.get(
+  '/persons/:personId/classrooms',
+  listClassroomsOfAPersonValidator,
+  personsController.listClassroomsOfAPerson
+);
+
+router.use('/persons', ensureIsASchoolMiddleware);
+
 router.post(
   '/persons',
-  ensureIsASchoolMiddleware,
   createUserValidator,
   createPersonValidator,
   personsController.store
 );
 
-router.get(
-  '/persons/:personId/classrooms',
-  listClassroomsOfAPersonValidator,
-  personsController.listClassroomsOfAPerson
+router.delete(
+  '/persons/:personId',
+  deleteAPersonValidator,
+  personsController.delete
 );
 
 export default router;
