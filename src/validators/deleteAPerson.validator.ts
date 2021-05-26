d1d10a3f3bb99a1210d49cdfbd953a089a6ff69b
DanielGustavo <dg520589@gmail.com>
2021-05-25 21:05:49 -0300

    feat(backend): create validator 'deleteAPersonValidator'

---
+++ src/validators/deleteAPerson.validator.ts
@@ -0,0 +1,23 @@
+import * as yup from 'yup';
+
+import { Request, Response, NextFunction } from 'express';
+
+import AppError from '../errors/AppError';
+
+export default async function deleteAPersonValidator(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const schema = yup.object().shape({
+    personId: yup.string().uuid().required(),
+  });
+
+  try {
+    await schema.validate(request.params);
+  } catch (error) {
+    throw new AppError(400, error.message);
+  }
+
+  return next();
+}
