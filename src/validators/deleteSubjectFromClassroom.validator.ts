ff8007bdca89e720d3d60d58231770368be3c259
DanielGustavo <dg520589@gmail.com>
2021-05-28 21:46:06 -0300

    feat(backend): create validator 'deleteSubjectFromClassroomValidator'

---
+++ src/validators/deleteSubjectFromClassroom.validator.ts
@@ -0,0 +1,24 @@
+import * as yup from 'yup';
+
+import { Request, Response, NextFunction } from 'express';
+
+import AppError from '../errors/AppError';
+
+export default async function deleteSubjectFromClassroomValidator(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const paramsSchema = yup.object().shape({
+    classroomId: yup.string().uuid().required(),
+    subjectId: yup.string().uuid().required(),
+  });
+
+  try {
+    await paramsSchema.validate(request.params);
+  } catch (error) {
+    throw new AppError(400, error.message);
+  }
+
+  return next();
+}
