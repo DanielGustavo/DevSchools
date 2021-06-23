e559907380df6a5fd620a89409b22c6be0dc10b2
DanielGustavo <dg520589@gmail.com>
2021-06-23 10:01:31 -0300

    feat(backend): limit 'getClassroomByIdService' to get only 5 persons and subjects from a classroom

--- src/services/getClassroomById.service.ts
+++ src/services/getClassroomById.service.ts
@@ -1,6 +1,8 @@
-import { getRepository } from 'typeorm';
+import { getConnection, getRepository } from 'typeorm';
 
 import Classroom from '../database/models/Classroom';
+import Person from '../database/models/Person';
+import Subject from '../database/models/Subject';
 
 import AppError from '../errors/AppError';
 
@@ -12,10 +14,7 @@ interface Request {
 export default async function getClassroomByIdService(request: Request) {
   const classroomRepository = getRepository(Classroom);
 
-  const classroom = await classroomRepository.findOne({
-    where: { id: request.classroomId },
-    relations: ['subjects', 'persons'],
-  });
+  const classroom = await classroomRepository.findOne(request.classroomId);
 
   if (!classroom) {
     throw new AppError(400, 'This classroom does not exist');
@@ -27,6 +26,34 @@ export default async function getClassroomByIdService(request: Request) {
   if (schoolDoesNotOwnThisClassroom) {
     throw new AppError(403, 'Your school does not own this classroom');
   }
+
+  classroom.subjects = await getConnection()
+    .createQueryBuilder(Subject, 'subject')
+    .leftJoinAndSelect(
+      'classrooms_subjects',
+      'cs',
+      'cs.classroom_id = :classroomId AND cs.subject_id = subject.id',
+      { classroomId: request.classroomId }
+    )
+    .take(5)
+    .where('cs.classroom_id = :classroomId', {
+      classroomId: request.classroomId,
+    })
+    .getMany();
+
+  classroom.persons = await getConnection()
+    .createQueryBuilder(Person, 'person')
+    .leftJoinAndSelect(
+      'persons_classrooms',
+      'pc',
+      'pc.classroom_id = :classroomId AND pc.person_id = person.id',
+      { classroomId: request.classroomId }
+    )
+    .take(5)
+    .where('pc.classroom_id = :classroomId', {
+      classroomId: request.classroomId,
+    })
+    .getMany();
 
   return classroom;
 }
