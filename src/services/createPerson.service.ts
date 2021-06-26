f53610a32463bc70dd60706fff886d09983c74f2
DanielGustavo <dg520589@gmail.com>
2021-06-26 11:16:00 -0300

    feat(backend/createPersonService): send email with an access link after registering the person
    after registering the person, this function send an email with an access link, which has a base64-encoded jwt in the url parameter 'token'

--- src/services/createPerson.service.ts
+++ src/services/createPerson.service.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { base64encode } from 'nodejs-base64';
 
 import Person from '../database/models/Person';
 import School from '../database/models/School';
@@ -8,13 +9,15 @@ import createUserService from './createUser.service';
 import AppError from '../errors/AppError';
 
 import generateRandomString from '../utils/generateRandomString';
+import createTokenToAUserOfTypePerson from '../utils/createTokenToAUserOfTypePerson';
+
+import mailer from '../modules/mailer';
 
 interface Request {
   personDatas: {
     email: string;
     role: string;
     name: string;
-    password: string;
   };
   schoolId: string;
 }
@@ -47,5 +50,19 @@ export default async function createPersonService(
   });
 
   await personRepository.save(person);
+
+  const token = createTokenToAUserOfTypePerson({ person, user });
+  const tokenBase64Encoded = base64encode(token);
+
+  const accessLink = `${process.env.APP_URL}/?token=${tokenBase64Encoded}`;
+
+  await mailer.send({
+    subject: 'Account created susscessfully!',
+    to: personDatas.email,
+    html: `<h1>Hi ${personDatas.name}, your account in Devschools was created susscessfully!</h1>
+    <p>Click <a href=${accessLink}>here</a> to login and setup your account</p>.
+    `,
+  });
+
   return person;
 }
