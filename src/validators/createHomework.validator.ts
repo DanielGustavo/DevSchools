709d46d69f68d5ccea792c55e54b1da06b62787f
DanielGustavo <dg520589@gmail.com>
2021-07-15 10:00:13 -0300

    feat(backend): create validator 'createHomeworkValidator'

---
+++ src/validators/createHomework.validator.ts
@@ -0,0 +1,26 @@
+import * as yup from 'yup';
+import { Request, Response, NextFunction } from 'express';
+
+import AppError from '../errors/AppError';
+
+export default async function createHomeworkValidator(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const schema = yup.object().shape({
+    deadline: yup.number().required(),
+    classroomId: yup.string().uuid().required(),
+    subjectId: yup.string().uuid().required(),
+    title: yup.string().min(5).max(100).required(),
+    description: yup.string().min(5).max(300),
+  });
+
+  try {
+    await schema.validate(request.body);
+  } catch (error) {
+    throw new AppError(400, error.message);
+  }
+
+  return next();
+}
