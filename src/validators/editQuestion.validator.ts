719b032653f651262b51014dafba835bae9c02e1
DanielGustavo <dg520589@gmail.com>
2021-07-21 21:18:29 -0300

    feat(backend): create validator 'editQuestionValidator'

---
+++ src/validators/editQuestion.validator.ts
@@ -0,0 +1,36 @@
+import * as yup from 'yup';
+import { Request, Response, NextFunction } from 'express';
+
+import AppError from '../errors/AppError';
+
+export default async function editQuestionValidator(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const alternativeSchema = yup.object().shape({
+    text: yup
+      .string()
+      .min(5, 'All alternatives must have at least 5 characters.')
+      .max(200, 'An alternative can not have more than 200 characters.'),
+    correct: yup
+      .boolean()
+      .typeError('The field "correct" in each alternative must be a boolean.'),
+    deleted: yup
+      .boolean()
+      .typeError('The field "deleted" in each alternative must be a boolean.'),
+  });
+
+  const schema = yup.object().shape({
+    questionTitle: yup.string().min(5).max(500),
+    alternatives: yup.array().of(alternativeSchema),
+  });
+
+  try {
+    await schema.validate(request.body);
+  } catch (error) {
+    throw new AppError(400, error.message);
+  }
+
+  return next();
+}
