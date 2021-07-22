b467bac5ca347e797f92c110e3d458647d658621
DanielGustavo <dg520589@gmail.com>
2021-07-21 21:18:08 -0300

    feat(backend): add method 'edit' in 'QuestionsController'

--- src/controllers/questions.controller.ts
+++ src/controllers/questions.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import deleteAQuestionService from '../services/deleteAQuestion.service';
+import updateAQuestionService from '../services/updateAQuestion.service';
 
 class QuestionsController {
   async delete(request: Request, response: Response) {
@@ -8,6 +9,21 @@ class QuestionsController {
       questionId: request.params.questionId,
       teacherId: request.user.person?.id as string,
     });
+
+    return response.json(question);
+  }
+
+  async edit(request: Request, response: Response) {
+    const { alternatives, questionTitle } = request.body;
+
+    const question = await updateAQuestionService({
+      alternatives,
+      questionTitle,
+      questionId: request.params.questionId,
+      teacherId: request.user.person?.id as string,
+    });
+
+    Object.assign(question, { homework: undefined });
 
     return response.json(question);
   }
