7cedfb7545d652ad791bc20192589f741932e2c5
DanielGustavo <dg520589@gmail.com>
2021-08-19 18:20:52 -0300

    feat(backend): add POST route '/students/:personId/homeworks/:homeworkId'

--- src/routes/students.routes.ts
+++ src/routes/students.routes.ts
@@ -1,18 +1,30 @@
 import { Router } from 'express';
 
 import ensureAuthorizationMiddleware from '../middlewares/ensureAuthorization.middleware';
+import ensureIsAStudentMiddleware from '../middlewares/ensureIsAStudent.middleware';
 
 import studentsController from '../controllers/students.controller';
 
 import hasPersonIdInParamsValidator from '../validators/hasPersonIdInParams.validator';
+import hasHomeworkIdInParamsValidator from '../validators/hasHomeworkIdInParams.validator';
+import answerHomeworkValidator from '../validators/answerHomework.validator';
 
 const router = Router();
 
-router.get(
+router.use(
   '/students/:personId',
   ensureAuthorizationMiddleware,
-  hasPersonIdInParamsValidator,
-  studentsController.listStudent
+  hasPersonIdInParamsValidator
+);
+
+router.get('/students/:personId', studentsController.listStudent);
+
+router.post(
+  '/students/:personId/homeworks/:homeworkId',
+  ensureIsAStudentMiddleware,
+  hasHomeworkIdInParamsValidator,
+  answerHomeworkValidator,
+  studentsController.answerAHomework
 );
 
 export default router;
