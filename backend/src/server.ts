83c7056692177cc62ef708c231d7186366903a25
DanielGustavo <dg520589@gmail.com>
2021-10-15 20:21:12 -0300

    feat(backend): add cors to let all websites access the api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,9 @@ import './database';
 import 'express-async-errors';
 import 'dotenv/config';
 import 'reflect-metadata';
+import cors from 'cors';
 import express from 'express';
+
 import routes from './routes';
 
 import handleAppErrorThrowMiddleware from './middlewares/handleAppErrorThrow.middleware';
@@ -11,6 +13,7 @@ import handleAppErrorThrowMiddleware from './middlewares/handleAppErrorThrow.mid
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 app.use(routes);
 app.use(handleAppErrorThrowMiddleware);
 
